1e5e020a019ae8a26f384c27bd28d01eb9594b54
Hariom <hariom@gmail.com>
2018-12-09 23:44:46 +0530

    rc

--- app/Http/Controllers/Api/ServiceController.php
+++ app/Http/Controllers/Api/ServiceController.php
@@ -299,7 +299,7 @@ class ServiceController extends Controller {
                 $serviceRequest->question_id = $request->question_id ? $request->question_id : 0;
                 $serviceRequest->request_status_id = 1;
                 if ($serviceRequest->save()) {
-                    $this->generateNotification($serviceRequest->user_id, "Order & Request created", "$service->name raised succesfully. Our staff will contact you soon.", 1);
+                    $this->generateNotification($serviceRequest->user_id, "Order & Request raised", "$service->name request raised succesfully. Our staff will contact you soon.", 1);
                     return $this->sendSuccessResponse("Request successfully created.", (object) []);
                 } else {
                     return $this->sendErrorResponse("Something went be wrong.", (object) []);
