0529798a3ea6139221ac17d7e36609dd308e675b
Hariom <hariom_gangwar@seologistics.com>
2018-12-21 14:42:34 +0530

    rc

--- app/Models/HealthcateProgramDay.php
+++ app/Models/HealthcateProgramDay.php
@@ -4,7 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class HealthcateProgramDay extends Model
-{
-    //
+class HealthcateProgramDay extends Model {
+
+    public function getDescriptionAttribute($name) {
+        return $name == null ? "" : $name;
+    }
+
 }
