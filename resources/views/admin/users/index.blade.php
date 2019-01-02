64b531e2ba01ccecc39b250a43f0198789f2d052
Hariom <hariom@gmail.com>
2019-01-02 22:30:18 +0530

    rc

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -42,7 +42,7 @@
 <script>
     $(document).ready(function () {
         var t = $('#list').DataTable({
-            lengthMenu: [[5, 10, 25, 50], [5, 10, 25, 50]],
+            lengthMenu: [[10, 15, 25, 50], [10, 15, 25, 50]],
             searching: true,
             processing: true,
             serverSide: true,
