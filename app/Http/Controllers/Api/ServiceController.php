2ca3cb0ebfade8aa01a019dcdafcf7201d2fdf6d
Hariom <hariom@gmail.com>
2019-01-03 01:11:54 +0530

    rc

--- app/Http/Controllers/Api/ServiceController.php
+++ app/Http/Controllers/Api/ServiceController.php
@@ -304,16 +304,18 @@ class ServiceController extends Controller {
             if ($existingServiceRequest) {
                 return $this->sendErrorResponse("Request already raised.", (object) []);
             } else {
-                $userDetail = User::where("id", $request->user_id)->with("userBookingDetail")->first();
-                $room = ResortRoom::find($userDetail->userBookingDetail->room_type_id);
-                $roomType = RoomType::find($userDetail->userBookingDetail->resort_room_id);
+                $userDetail = User::where("id", $request->user_id)->with([
+                        'userBookingDetail' => function($query) {
+                            $query->selectRaw(DB::raw('id, room_type_id, resort_room_id, user_id, source_id as booking_id, source_name, resort_id, package_id, DATE_FORMAT(check_in, "%d-%b-%Y") as check_in, DATE_FORMAT(check_in, "%r") as check_in_time, DATE_FORMAT(check_out, "%d-%b-%Y") as check_out, DATE_FORMAT(check_out, "%r") as check_out_time'));
+                        }
+                    ])->first();
 
                 $serviceRequest = new ServiceRequest();
                 $serviceRequest->resort_id = $request->resort_id;
                 $serviceRequest->user_id = $request->user_id;
                 $serviceRequest->service_id = $request->service_id;
-                $serviceRequest->room_type_name = $roomType ? $roomType->name : "";
-                $serviceRequest->resort_room_no = $room ? $room->room_no : "";
+                $serviceRequest->room_type_name = $userDetail->userBookingDetail->room_type_detail ? $userDetail->userBookingDetail->room_type_detail->name : "";
+                $serviceRequest->resort_room_no = $userDetail->userBookingDetail->room_detail ? $userDetail->userBookingDetail->room_detail->room_no : "";
                 $serviceRequest->comment = $request->comment ? $request->comment : '';
                 $serviceRequest->question_id = $request->question_id ? $request->question_id : 0;
                 $serviceRequest->request_status_id = 1;
