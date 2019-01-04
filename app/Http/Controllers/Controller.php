0690cff8b51dbe8906d6e4596962624cf3837d21
Hariom <hariom@gmail.com>
2019-01-04 23:02:47 +0530

    rc

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -109,14 +109,13 @@ class Controller extends BaseController {
             'accusage' => 1
         );
         $ch = curl_init();
-
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, count($fields));
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($fields));
-        $result = curl_exec($ch);
+        curl_exec($ch);
         curl_close($ch);
 
-        return $result;
+        return true;
     }
 
 }
