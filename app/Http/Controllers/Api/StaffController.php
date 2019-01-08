e5ec49c08f21051d285385a4c3cce0ae881c480d
Hariom <hariom_gangwar@seologistics.com>
2019-01-08 14:14:54 +0530

    rc

--- app/Http/Controllers/Api/StaffController.php
+++ app/Http/Controllers/Api/StaffController.php
@@ -437,8 +437,8 @@ class StaffController extends Controller {
                 $ongoingJobArray[$i]["room_no"] = $ongoing_job->resort_room_no;
                 $ongoingJobArray[$i]["created_at"] = $created_at->format('d-m-Y h:i a');
                 $ongoingJobArray[$i]["type"] = 1;
-                $ongoingJobArray[$i]["staff_comment"] = $ongoing_job->staff_comment;
-                $ongoingJobArray[$i]["staff_reasons"] = $ongoing_job->staff_reasons;
+                $ongoingJobArray[$i]["staff_comment"] = $ongoing_job->staff_comment ? $ongoing_job->staff_comment : "";
+                $ongoingJobArray[$i]["staff_reasons"] = $ongoing_job->staff_reasons ? $ongoing_job->staff_reasons : "";
                 $i++;
             }
 
@@ -501,7 +501,7 @@ class StaffController extends Controller {
                 $i++;
             }
 
-            $under_approval_jobs = ServiceRequest::select('id', 'comment', 'service_id', 'request_status_id', 'user_id', 'room_type_name', 'resort_room_no')->where(["accepted_by_id" => $request->user()->id, "request_status_id" => 3, "is_active" => 1])
+            $under_approval_jobs = ServiceRequest::select('id', 'comment', 'staff_reasons', 'staff_comment', 'service_id', 'request_status_id', 'user_id', 'room_type_name', 'resort_room_no')->where(["accepted_by_id" => $request->user()->id, "request_status_id" => 3, "is_active" => 1])
                     ->with([
                         'serviceDetail' => function($query) {
                             $query->select('id', 'name', 'icon', 'type_id');
@@ -534,6 +534,8 @@ class StaffController extends Controller {
                 $underApprovalJobArray[$j]["room_no"] = $under_approval_job->resort_room_no;
                 $underApprovalJobArray[$j]["created_at"] = $created_at->format('d-m-Y h:i a');
                 $underApprovalJobArray[$j]["type"] = 1;
+                $underApprovalJobArray[$j]["staff_reasons"] = $under_approval_job->staff_reasons ? $under_approval_job->staff_reasons : "";
+                $underApprovalJobArray[$j]["staff_comment"] = $under_approval_job->staff_comment ? $under_approval_job->staff_comment : "";
                 $j++;
             }
 
