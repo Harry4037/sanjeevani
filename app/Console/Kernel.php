b91b49d8f3f9b1894b89da5283d9ab05daf27715
Hariom <hariom@gmail.com>
2019-05-29 12:15:52 +0530

    rc

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,42 +28,45 @@ class Kernel extends ConsoleKernel {
      * @return void
      */
     protected function schedule(Schedule $schedule) {
-//        $schedule->call(function () {
-//            $tokens = DB::table('users')->where(["is_active" => 1, "user_type_id" => 3])
-//                    ->where("device_token", "!=", NULL)
-//                    ->orWhere("device_token", "!=", "")
-//                    ->pluck("device_token");
-//            if ($tokens) {
-//                config(['fcm.http.server_key' => 'AAAAZDeprME:APA91bHyGVMy54RTPTZKyj-gsF5L31IsHP0efkEm4RorsITp-yH2Syh-ftIuuaIu2zm7zZpJZp_CBmY4B33yahx1uZWG570_z6bJ9OxnuX2_Zzh9NFwVbtYKANXRh7SpsQZPq328Y-Jj']);
-//                config(['fcm.http.sender_id' => '430430596289']);
-//
-//
-//                $optionBuilder = new OptionsBuilder();
-//                $optionBuilder->setTimeToLive(60 * 20)
-//                        ->setPriority('high');
-//
-//                $notificationBuilder = new PayloadNotificationBuilder("Reminder Push");
-//                $notificationBuilder->setBody("Booking reminder notification.")
-//                        ->setSound('soundn.mp3');
-//
-//                $dataBuilder = new PayloadDataBuilder();
-//                $dataBuilder->addData([
-//                    'title' => "Reminder Push",
-//                    'message' => "Booking reminder notification.",
-//                    "type" => 234,
-//                    "user_type_id" => 3,
-//                    "sound" => "soundn.mp3",
-//                ]);
-//
-//                $option = $optionBuilder->build();
-//                $notification = $notificationBuilder->build();
-//                $data = $dataBuilder->build();
-//
-//                $token = "cj2GrsTHQlU:APA91bGw707gSY_IYpVY0_cYHm8GiBVslMc86er03xkNr8_ixiuyN95OmVH0ctLSv9JOjq5acIHjKnWfq_fx0yxw3KidSkSrVdHx2TWjFBzaJhwpt72B6IcB0UN24G_fBbsy3f4OPw-K";
-//
-//                $downstreamResponse = FCM::sendTo([$token], $option, $notification, $data);
-//            }
-//        })->everyMinute();
+        $schedule->call(function () {
+            $next2days = date('Y-m-d H:i:s', strtotime("+2 days"));
+            $next2days24Hour = date('Y-m-d H:i:s', strtotime("+24 hours", strtotime($next2days)));
+
+            $userBookings = DB::table('user_booking_details')->whereBetween("check_in", [$next2days, $next2days24Hour])->get();
+            foreach ($userBookings as $userBooking) {
+                $user = DB::table('users')->find($userBooking->user_id);
+                if ($user->device_token) {
+                    config(['fcm.http.server_key' => 'AAAAZDeprME:APA91bHyGVMy54RTPTZKyj-gsF5L31IsHP0efkEm4RorsITp-yH2Syh-ftIuuaIu2zm7zZpJZp_CBmY4B33yahx1uZWG570_z6bJ9OxnuX2_Zzh9NFwVbtYKANXRh7SpsQZPq328Y-Jj']);
+                    config(['fcm.http.sender_id' => '430430596289']);
+
+
+                    $optionBuilder = new OptionsBuilder();
+                    $optionBuilder->setTimeToLive(60 * 20)
+                            ->setPriority('high');
+
+                    $notificationBuilder = new PayloadNotificationBuilder("Reminder Notification");
+                    $notificationBuilder->setBody("You upcoming booking date is " . date("d-m-Y", strtotime($userBooking->check_in)))
+                            ->setSound('soundn.mp3');
+
+                    $dataBuilder = new PayloadDataBuilder();
+                    $dataBuilder->addData([
+                        'title' => "Reminder Notification",
+                        'message' => "You upcoming booking date is " . date("d-m-Y", strtotime($userBooking->check_in)),
+                        "type" => 234,
+                        "user_type_id" => 3,
+                        "sound" => "soundn.mp3",
+                    ]);
+
+                    $option = $optionBuilder->build();
+                    $notification = $notificationBuilder->build();
+                    $data = $dataBuilder->build();
+
+                    $token = "cj2GrsTHQlU:APA91bGw707gSY_IYpVY0_cYHm8GiBVslMc86er03xkNr8_ixiuyN95OmVH0ctLSv9JOjq5acIHjKnWfq_fx0yxw3KidSkSrVdHx2TWjFBzaJhwpt72B6IcB0UN24G_fBbsy3f4OPw-K";
+
+                    $downstreamResponse = FCM::sendTo($token, $option, $notification, $data);
+                }
+            }
+        })->everyMinute();
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::namespace("Admin")->prefix('admin')->group(function() {
 
 
 Route::namespace("Admin")->prefix('admin')->middleware(['adminGuest'])->group(function() {
+    Route::match(['get', 'post'], '/test', 'LoginController@test')->name('admin.test');
     Route::match(['get', 'post'], '/profile', 'LoginController@profile')->name('admin.profile');
     Route::match(['get', 'post'], '/change-password', 'LoginController@changePassword')->name('admin.change-password');
     Route::get('/city-list/{id}', 'CommonController@getCityList')->name('admin.city.list');
