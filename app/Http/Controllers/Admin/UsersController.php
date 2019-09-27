1d589f96a6fd467611477b32213b6665213abc22
Hariom <hariom@gmail.com>
2019-09-27 11:25:15 +0530

    rc

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -738,10 +738,13 @@ class UsersController extends Controller {
             $roomType = RoomType::find($request->resort_room_type);
             $roomRoom = ResortRoom::find($request->resort_room_id);
             $msg = "";
+            $flag = FALSE;
             if ($data->room_type_name != $roomRoom->room_no) {
                 $msg = "Your room number updated successfully.";
+                $flag = TRUE;
             } else {
                 $msg = "Your booking updated successfully.";
+                $flag = FALSE;
             }
 //            $data->discount = $request->discount;
             $data->source_name = $request->booking_source_name;
@@ -777,8 +780,12 @@ class UsersController extends Controller {
                 }
 
                 if ($user->device_token) {
-                    $this->androidBookingPushNotification("Booking Updated", $msg, $user->device_token, $this->notificationCount($user->id));
                     $this->generateNotification($user->id, "Booking Updated", $msg, 5);
+                    if ($flag) {
+                        $this->androidPushNotification(3, "Booking Updated", $msg, $user->device_token, 123, 0);
+                    } else {
+                        $this->androidBookingPushNotification("Booking Updated", $msg, $user->device_token, $this->notificationCount($user->id));
+                    }
                 }
                 return redirect()->route('admin.users.booking-edit', $data->id)->with('status', $msg);
             } else {
