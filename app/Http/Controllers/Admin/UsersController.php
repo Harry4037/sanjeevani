6e9e961c33c7c0209f8a2c33739ba1728d8fdd7b
Hariom <hariom@gmail.com>
2019-12-02 17:02:02 +0530

    rc

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -665,7 +665,7 @@ class UsersController extends Controller {
             }
 
             $user = User::find($user_id);
-            $isCurrentBooking = $this->isCurrentBooking($user->id);
+            $isCurrentBooking = $this->isCurrentBooking($request->check_in);
             if (!$user) {
                 return redirect()->route('admin.users.booking-create', $user_id)->withErrors("user not found.")->withInput();
             }
